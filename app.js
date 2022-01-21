0c3dcc92a535d672e83239c1d86856dc41273e43
ags4436 <kvnaakash01@gmail.com>
2022-01-21 14:25:51 +0530

    expressStart

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Test Merge Behind Commit")
+const express=require('express');
+const app=express();
+
+app.get("/",(req,res)=>{
+    res.setHeader('content-type','text/html')
+    res.status(200).send("<h1>Home Page</h1>");
+})
+
+app.get("/about",(req,res)=>{
+    res.status(200).send("about Page");
+})
+
+
+
+app.listen(5000,()=>{
+    console.log("Listening To port 5000!")
+})
