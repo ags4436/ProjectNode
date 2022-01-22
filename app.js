38b8c8d8fcd100dd7d90b14fd6900598ce131212
ags4436 <kvnaakash01@gmail.com>
2022-01-22 22:44:40 +0530

    MagicKeyUpdate

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 const express=require('express');
 const path=require('path');
+var bodyParser = require('body-parser');
 const {products}=require("./data");
+const { exec } = require("child_process");
 const app=express();
 
-//app.use(express.static("./public"));
 
- app.get("/",(req,res)=>{
-   res.json(products);
+let validateKey=((req,res,next)=>{
+    const key=req.body.key
+    let verify="";
+    exec("python v.py "+key, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(stdout);
+    });
+    console.log(verify);
+    if(verify!="Entered key is valid!"){
+    return res.send("Invalid Key!")
+    }
+
+    console.log("Success")
+    next()
+})
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static("./public"));
+
+ app.post("/submit",validateKey,(req,res)=>{
+    console.log(req.body.key)
+    res.send("Checking..")
 })
 
 app.get("/about",(req,res)=>{
@@ -21,3 +50,4 @@ app.all("*",(req,res)=>{
 app.listen(5000,()=>{
     console.log("Listening To port 5000!")
 })
+
